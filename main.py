66c01b413b57d51c1eb0ec36d6e793aa21be99e9
Ian Rocha <ianrocha.df@gmail.com>
2019-08-30 11:53:07 -0300

    Adição de nome e ano em listas independentes

--- main.py
+++ main.py
@@ -4,24 +4,41 @@ import random
 def nomes_reader():
     with open('nomes.csv') as csvfile:
         nomesCSV = csv.reader(csvfile, delimiter=',')
-        for row in nomesCSV:
-            print(row[1])
+        aux = random.randint(1,100)
+        auxs = str(aux)
+        for x in nomesCSV:
+            if auxs == x[0]:
+                return x[1]
 
 def cpfs_reader():
     with open('cpfs.csv') as csvfile:
         cpfsCSV = csv.reader(csvfile, delimiter=',')
         for row in cpfsCSV:
-            print(row)
+            return row[1]
 
 def random_date():
-    day = random.randint(1,31)
-    month = random.randint(1,12)
+    # day = random.randint(1,31)
+    # month = random.randint(1,12)
     year = random.randint(1980, 2019)
 
-    print(day,"/",month,"/",year)
+    return year
         
 
-#nomes_reader()
-#cpfs_reader()
-for x in range(50):
-    random_date()
+# nomes_reader()
+# cpfs_reader()
+# for x in range(50):
+#     random_date()
+
+person = []
+date = []
+for k in range(51):
+    person.append(nomes_reader())
+    date.append(random_date())
+
+teste = 1
+while teste <= 50:
+    print(person[teste])
+    print(date[teste])
+    teste += 1
+
+    
